ec5ec51238941d9f9aca64a95ce9f112ae95c9dd
Myattunoo <myattunoo1083@gmail.com>
2020-08-17 14:45:27 +0630

    test

---
+++ database/migrations/2020_08_14_083159_create_interns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInternsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interns', function (Blueprint $table) {
+            $table->biginteger('id');
+            $table->string('name');
+            $table->integer('age');
+            $table->string('gendar');
+            $table->string('DOB');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interns');
+    }
+}
